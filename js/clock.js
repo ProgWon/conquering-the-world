ed493ab57f1d10e45e62faa8180b5453ac0bd9ef
DongGunYoon <donggun961108@gmail.com>
2021-06-22 15:16:20 +0900

    fix: Fix some minor problems

--- js/clock.js
+++ js/clock.js
@@ -1,10 +1,10 @@
 const worldClock = (() => {
     const timeDiff = {
-        ko: 9,
-        us: -4,
-        ja: 9,
-        ca: -4,
-        ch: 8
+        KR: 9,
+        US: -4,
+        JP: 9,
+        CA: -4,
+        CH: 8
     };
 
     const formatTime = time => (time < 10) ? "0" + time : time;
@@ -12,7 +12,7 @@ const worldClock = (() => {
     setInterval(() => {
         const date = new Date();
         const utcTime = date.getTime() + (date.getTimezoneOffset() * 60 * 1000);
-        const currTime = new Date(utcTime + (timeDiff.ko * 60 * 60 * 1000));
+        const currTime = new Date(utcTime + (timeDiff.KR * 60 * 60 * 1000));
         const hour = formatTime(currTime.getHours());
         const minute = formatTime(currTime.getMinutes());
         const second = formatTime(currTime.getSeconds());
